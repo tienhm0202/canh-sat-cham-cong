41f322fc0cb6aae178c63c2cd4b19abbba116a98
Hoang Manh Tien <tienhm@eway.vn>
2021-05-13 22:17:26 +0700

    rem: configs

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 # Slack
-SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "xoxb-461422697699-1023036851697-9Yh3xUrnolqBXN9djZ21vI8F")
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_DEFAULT_CHANNEL = os.environ.get("SLACK_DEFAULT_CHANNEL", "#test_bot")
 SLACK_PATTERN = {
     "daily": ["[daily]"],
@@ -15,7 +15,7 @@ SLACK_PATTERN = {
 SLACK_REACTION_EMOJI = os.environ.get("SLACK_REACTION_EMOJI", "+1")
 
 # AirTable
-AIRTABLE_TOKEN = os.environ.get("AIRTABLE_TOKEN", "keyWk5bt8Enu6ZPA0")
-AIRTABLE_BASE = os.environ.get("AIRTABLE_BASE", "appp7hAGJr6m0rFqo")
+AIRTABLE_TOKEN = os.environ.get("AIRTABLE_TOKEN")
+AIRTABLE_BASE = os.environ.get("AIRTABLE_BASE")
 AIRTABLE_TABLE_CHECKIN = os.environ.get("AIRTABLE_TABLE_CHECKIN", "Checkin")
 AIRTABLE_TABLE_ABSENT = os.environ.get("AIRTABLE_TABLE_ABSENT", "Absent")
